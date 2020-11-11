e676efc12f8e7b559b830f5f6af6af3c34b0a784
Krish Desai <71713595+krishdesai62@users.noreply.github.com>
2020-11-11 22:07:00 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,92 +1,159 @@
-from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth.forms import UserCreationForm
-from datetime import datetime
-from home.models import Contact
-from home.models import Login
-from home.models import Signup
+from django.shortcuts import render,HttpResponse
+import datetime
 from django.contrib import messages
-from .forms import CreateUserForm
-
-from django.contrib.auth.decorators import login_required
-
-from django.contrib.auth import authenticate, login,logout
+from .models import *
+import json
+from django.http import JsonResponse
+from .utils import cookieCart,cartData,guestOrder
 
 # Create your views here.
-@login_required(login_url='login')
+
 def index(request):
-    context = {
-        "variable":"this is all about me"
-    }
-   # return HttpResponse('this is home page')
-    return render(request,'index.html', context)
-
-@login_required(login_url='login')
-def products(request):
-    return render(request,'products.html')
-
-@login_required(login_url='login')
-def contact(request):
-    return render(request,'contact.html')
+    return render(request,'index.html')
 
 def register(request):
+    if request.method == "POST":
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        number = request.POST.get('number')
+        marital_status = request.POST.get('marital_status')
+        language = request.POST.get('language')
+        job = request.POST.get('job')
+        religion = request.POST.get('religion')
+        pan_card = request.POST.get('pan_card')
+        aadhar_card = request.POST.get('aadhar_card')
+        photo = request.POST.get('photo')
+
+        sitter = Sitter(fname=fname, lname=lname, email=email, password=password, address=address, city=city, state=state, zipcode=zipcode, age=age, gender=gender, number=number, marital_status=marital_status, language=language, job=job, religion=religion, pan_card=pan_card, aadhar_card=aadhar_card, photo=photo)
+
+        sitter.save()
     return render(request,'register.html')
 
-@login_required(login_url='login')
+def login(request):
+    return render(request,'login.html')
+
+def signin(request):
+    return render(request,'signin.html')
+
+def baby(request):
+    return render(request,'baby.html')
+
 def cook(request):
     return render(request,'cook.html')
 
-@login_required(login_url='login')
 def elderly(request):
     return render(request,'elderly.html')
 
-@login_required(login_url='login')
 def about(request):
     return render(request,'about.html')
-
-@login_required(login_url='login')
+    
 def services(request):
     return render(request,'services.html')
 
-def loginpage(request):
-	if request.user.is_authenticated:
-		return redirect('home')
-	else:
-            if request.method == 'POST':
-                username = request.POST.get('username')
-                password =request.POST.get('password')
-
-                user = authenticate(request, username=username, password=password)
-
-                if user is not None:
-                    login(request, user)
-                    messages.success(request, 'Successfully logged in')
-                    return redirect('/')
-                else:
-                    messages.info(request, 'Username Or password is incorrect')
-            context = {}
-            return render(request, 'login.html', context)
+def contact(request):
+    return render(request,'contact.html')
 
 
-def logoutUser(request):
-    logout(request)
-    messages.warning(request,"Account successfully logout")
-    return redirect('login')
+
+def store(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+
+    products=Product.objects.all()
+    context={'products':products,'cartItems':cartItems}
+    return render(request,'store.html',context)
 
 
-def signup(request):
-	if request.user.is_authenticated:
-		return redirect('home')
-	else:
-		form = CreateUserForm()
-		if request.method == 'POST':
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
 
-				return redirect('login')
-			
+def cart(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
 
-		context = {'form':form}
-		return render(request, 'signup.html', context)
+    context={'items':items,'order':order,'cartItems':cartItems}    
+    return render(request,'cart.html',context)    
+
+
+
+def checkout(request):  
+
+    data = cartData(request)
+    cartItems=data['cartItems']
+    order=data['order']
+    items=data['items']
+
+    context={'items':items,'order':order,'cartItems':cartItems}     
+    return render(request,'checkout.html',context)    
+
+
+
+def updateItem(request):
+    data=json.loads(request.body)
+    productId=data['productId']
+    action=data['action']
+
+    print('Action:',action)
+    print('productId:',productId)
+
+    customer=request.user.customer
+    product=Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created=OrderItem.objects.get_or_create(order=order,product=product)
+
+    if action == 'add':
+        orderItem.quantity(orderItem.quantity+1)
+    elif action == 'remove':
+        orderItem.quantity(orderItem.quantity-1)
+
+    orderItem.save()
+
+    if orderItem.quantity <=0:
+        orderItem.delete()        
+
+    return JsonResponse('Item was added',safe=False)
+
+
+def processOrder(request):
+    transaction_id=datetime.datetime.now().timestamp()
+    data=json.loads(request.body)
+    
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        
+
+        
+
+    else:
+        customer,order=guestOrder(request,data)
+        
+
+    total=float(data['form']['total'])
+    order.transaction_id=transaction_id
+
+    if total == order.get_cart_total:
+        order.complete=True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer, 
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+
+                    )
+
+    return JsonResponse('Payment completed',safe=False)
